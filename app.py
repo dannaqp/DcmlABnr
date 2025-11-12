649668e8839512951ae7b54fa9f07e1735e0402a
dannaqp <dasimaluisaqu@uide.edu.ec>
2025-11-12 00:30:28 +0000

    feat: add decimal to binary converter

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
-#-----------------------------------------------------------------------------------------
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License. See LICENSE in the project root for license information.
-#-----------------------------------------------------------------------------------------
+# Proyecto Parcial 2
+# Ariel Guerrero, Cesar Arciniegas, Camila Villagran y Danna Simaluisa
+# Decimal de 2 cifras a numero binario de 8 bits
 
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-    return app.send_static_file("index.html")
+print('CONVERSOR DE DECIMAL A BINARIO DE 8 BITS')
+print()
+nrodec = int(input('Ingrese su numero decimal de dos cifras: '))
+while nrodec <= 0 or nrodec >= 100:
+    nrodec = int(input('Error: El número debe ser de 0 a 99 (2 cifras): '))    
+else:
+    numdec = nrodec
+    resultado = ""
+    valores = (128, 64, 32, 16, 8, 4, 2, 1)
+    for i in range(8):
+        if numdec >= valores[i]:
+            resultado = resultado + "1"
+            numdec = numdec - valores[i]
+        else:
+            resultado = resultado + "0"
+    print()
+    print('El equivalente binario de' , nrodec , 'es: ' , resultado)
+    print()
